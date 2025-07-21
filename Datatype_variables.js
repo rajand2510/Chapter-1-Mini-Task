5d5894d09131600f52e0bbbf0f4da13ed2327a27
Rajan Dhariyaparmar <rajandalvadi2510@gmail.com>
2025-07-21 12:04:07 +0530

    task 2 added

---
+++ Datatype_variables.js
@@ -0,0 +1,17 @@
+const persons = {
+   person1: {
+       name: 'Rajan',
+       age: 21,
+       city: 'Gandhinagar'
+   },
+    person2: {
+         name: 'Harsh',
+         age: 23,
+         city: 'Ahmedabad'
+    },
+}
+
+console.log(`Person details:`);
+console.log(`Name: ${persons.person1.name}, Age: ${persons.person1.age}, City: ${persons.person1.city}`);
+console.log(`Name: ${persons.person2.name}, Age: ${persons.person2.age}, City: ${persons.person2.city}`);
+
